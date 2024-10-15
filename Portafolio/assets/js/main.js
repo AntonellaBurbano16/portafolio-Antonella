f47dec7d204c1fc5a8665c24ac541f69c672a4f4
AntonellaBurbano16 <antonellaburbano.n@gmail.com>
2024-10-14 20:00:12 -0500

    ajustes portafolio

--- Portafolio/assets/js/main.js
+++ Portafolio/assets/js/main.js
@@ -1,11 +1,16 @@
 // Navigation Bar Function
 function myMenuFunction() {
 	var menuBtn = document.getElementById("myNavMenu");
-
+	var navListBtn = document.getElementsByClassName("nav-link");
 	if (menuBtn.className === "nav-menu") {
 		menuBtn.className += " responsive";
 	} else {
 		menuBtn.className = "nav-menu";
+	}
+	if (navListBtn.className === "nav-link") {
+		navListBtn.className += " responsive";
+	} else {
+		navListBtn.className = "nav-link";
 	}
 }
 
@@ -27,3 +32,22 @@ function headerShadow() {
 		navHeader.style.lineHeight = "90px";
 	}
 }
+
+//  Skills
+const skillsContent = document.getElementsByClassName("skills__content"),
+	skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+	let itemClass = this.parentNode.className;
+
+	for (i = 0; i < skillsContent.length; i++) {
+		skillsContent[i].className = "skills__content skills__close";
+	}
+	if (itemClass === "skills__content skills__close") {
+		this.parentNode.className = "skills__content skills__open";
+	}
+}
+
+skillsHeader.forEach((el) => {
+	el.addEventListener("click", toggleSkills);
+});
